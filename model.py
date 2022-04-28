cfa605a7681b7eb190baaa80665c5748b14786a8
Teemolition <alex.ludwig.s@web.de>
2022-04-29 00:24:08 +0200

    finished Model initialization & started implementing forwarding & get_action method

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch
 
 
 class Model(nn.Module):
@@ -8,19 +9,35 @@ class Model(nn.Module):
 
         self.input_dim = input_dim
         self.conv = nn.Sequential(
-            self.conv1 = nn.Conv2d(in_channels=input_dim, out_channels=32, kernel_size=8, stride=4)
-            self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
-            self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1)
+            nn.Conv2d(in_channels=input_dim, out_channels=32, kernel_size=8, stride=4), nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2), nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1), nn.ReLU()
         )
         self.value = nn.Sequential(
-            nn.Linear(in_channels= 64, out_channels= 512)
+            #nn.Linear(in_channels= 64, out_channels= 512), nn.ReLU(),   #TODO: NoisyLinear Klasse erstellen
+            #nn.NoisyLinear(512,1)
+            nn.Linear(in_channels= 64, out_channels= 512), nn.ReLU(),
+            nn.Linear(512,1)
         )
 
         self.advantage = nn.Sequential(
-            nn.Linear(in_channels= 64, out_channels=512)
-
+            #nn.NoisyLinear(in_channels= 64, out_channels=512), nn.ReLU(),      #TODO: NoisyLinear Klasse erstellen
+            #nn.NoisyLinear(512,action_space)
+            nn.Linear(in_channels= 64, out_channels=512), nn.ReLU(),
+            nn.Linear(512,action_space)
         )
 
-        # TODO
+    def forward(self, input):
+        value = self.value(self.conv(input))
+        advantage = self.advantage(self.conv(input))
+        Q = value + advantage - torch.mean(advantage, dim=1, keepdim=True)
+        return Q
+
+
+    def get_action(self, state):
+        with torch.no_grad():
+            Q = self.forward(state)
+            action_index = torch.argmax(Q, dim=1)
+        return action_index.item()
 
     # TODO

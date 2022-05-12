9476c552318bed7d2b775d1df0e7a1b97d4d514d
sjvie <viegener.soeren@googlemail.com>
2022-05-12 11:55:19 +0200

    replay buffer small additions

--- replay_buffer.py
+++ replay_buffer.py
@@ -1,28 +1,39 @@
+import random
+from collections import deque
+
+
 class ReplayBuffer():
 
-    def __init__(self):
+    def __init__(self, size):
+        self.size = size
+        self.memory = deque([], maxlen=size)
+
+    def add(self, state, action, reward, next_state, done):
+        """ Add new experience to the buffer"""
+        self.memory.append((state, action, reward, next_state, done))
+
+    def get_batch(self, batch_size=32):
+        return random.sample(self.memory, batch_size)
+
+    def save(self, folder_name):
         # TODO
         pass
 
-    def add(self,state, action, reward, next_state, done):
-        """ Add new experience to the buffer"""
-        pass
-    # TODO
-
-    def get_batch(self, batch_size=32):
-        pass
-    # TODO
-
-    def save(self, folder_name):
-        pass
-
     def load(self, folder_name):
+        # TODO
         pass
 
-class PrioritzedBuffer():
+class PrioritzedBuffer(ReplayBuffer):
     # TODO: Nochmal schauen, welche Methoden wir hier noch brauchen
     def __init__(self):
-        super(ReplayBuffer,self).__init__()
+        super(ReplayBuffer, self).__init__()
         pass
+
+    def get_batch(self, batch_size=32):
+        """ sample batch from memory using a KL loss-based distribution """
+        # TODO
+        pass
+
     # TODO
+
 

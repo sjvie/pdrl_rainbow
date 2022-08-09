44d8fef254043e640abebd750d724e21b6b3d827
Teemolition <alex.ludwig.s@web.de>
2022-08-10 00:07:40 +0200

    changed to no dist

--- configs/rainbow1_exp.py
+++ configs/rainbow1_exp.py
@@ -7,7 +7,7 @@ class Config(config.Config):
     use_per = True
     multi_step_n = 3
     use_noisy = True
-    use_distributional = True
+    use_distributional = False
     use_dueling = True
     use_double = True
     use_kl_loss = True
